eaf8372cfd92d36edd67f309a9408047b927e63c
krowqa2k <105343696+krowqa2k@users.noreply.github.com>
2024-07-13 16:19:45 +0200

    Initial Commit

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by admin on 13/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
