31c416cf243e7de16ea888b9a0e84e9adcc9ca8a
krowqa2k <105343696+krowqa2k@users.noreply.github.com>
2024-07-14 16:30:26 +0200

    feat
    added coinrow for list and added it to home view

--- CryptoTracker/Core/Home/Views/CoinRowView.swift
+++ CryptoTracker/Core/Home/Views/CoinRowView.swift
@@ -8,11 +8,69 @@
 import SwiftUI
 
 struct CoinRowView: View {
+    
+    let coin: CoinModel
+    let showHoldingsColumn: Bool
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing: 0) {
+            leftColumn
+            
+            Spacer()
+            
+            if showHoldingsColumn {
+                centerColumn
+            }
+            
+            rightColumn
+        }
+        .font(.subheadline)
     }
 }
 
-#Preview {
-    CoinRowView()
+struct CoinRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoinRowView(coin: dev.coin, showHoldingsColumn: true)
+    }
+}
+
+extension CoinRowView {
+    
+    private var leftColumn: some View {
+        HStack(spacing: 0) {
+            Text("\(coin.rank)")
+                .font(.caption)
+                .foregroundStyle(Color.theme.secondaryText)
+                .frame(minWidth: 30)
+            Circle()
+                .frame(width: 30, height: 30)
+            Text(coin.symbol.uppercased())
+                .font(.headline)
+                .padding(.leading)
+                .foregroundStyle(Color.theme.accent)
+        }
+    }
+    
+    private var centerColumn: some View {
+        VStack(alignment: .trailing) {
+            Text(coin.currentHoldingValue.asCurrencyWith6Decimals())
+                .bold()
+            Text((coin.currentHoldings ?? 0).asNumberString())
+        }
+        .foregroundStyle(Color.theme.accent)
+    }
+    
+    private var rightColumn: some View {
+        VStack(alignment: .trailing) {
+            Text(coin.currentPrice.asCurrencyWith2Decimals())
+                .bold()
+                .foregroundStyle(Color.theme.accent)
+            Text(coin.priceChangePercentage24H?.asPercentString() ?? "")
+                .foregroundStyle(
+                    (coin.priceChangePercentage24H ?? 0) >= 0 ?
+                    Color.theme.green : Color.theme.red
+                )
+        }
+        .frame(width: UIScreen.main.bounds.width / 3.5, alignment: .trailing)
+    }
 }
--- CryptoTracker/Core/Home/Views/HomeView.swift
+++ CryptoTracker/Core/Home/Views/HomeView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct HomeView: View {
     
+    @EnvironmentObject private var vm: HomeViewModel
     @State private var showPortfolio: Bool = false
     
     var body: some View {
@@ -16,6 +17,21 @@ struct HomeView: View {
             homeHeader
                 .padding(.horizontal)
                 Spacer(minLength: 0)
+            
+            columnTitles
+                .font(.caption)
+                .foregroundStyle(Color.theme.secondaryText)
+                .padding(.horizontal)
+            
+            if !showPortfolio {
+                allCoinsList
+                    .transition(.move(edge: .leading))
+            }
+            if showPortfolio {
+                portfolioCoinsList
+                    .transition(.move(edge: .trailing))
+            }
+            
         }
         .background(Color.theme.background)
     }
@@ -23,6 +39,7 @@ struct HomeView: View {
 
 #Preview {
     HomeView()
+        .environmentObject(HomeViewModel())
 }
 
 extension HomeView {
@@ -47,4 +64,35 @@ extension HomeView {
         }
     }
     
+    private var allCoinsList: some View {
+        List {
+            ForEach(vm.allCoins) { coin in
+                CoinRowView(coin: coin, showHoldingsColumn: false)
+                    .listRowInsets(.init(top: 10, leading: 0, bottom: 10, trailing: 10))
+            }
+        }
+        .listStyle(InsetListStyle())
+    }
+    
+    private var portfolioCoinsList: some View {
+        List {
+            ForEach(vm.portfolioCoins) { coin in
+                CoinRowView(coin: coin, showHoldingsColumn: true)
+                    .listRowInsets(.init(top: 10, leading: 0, bottom: 10, trailing: 10))
+            }
+        }
+        .listStyle(InsetListStyle())
+    }
+    
+    private var columnTitles: some View {
+        HStack {
+            Text("Coin")
+            Spacer()
+            if showPortfolio {
+                Text("Holdings")
+            }
+            Text("Price")
+                .frame(width: UIScreen.main.bounds.width / 3.5, alignment: .trailing)
+        }
+    }
 }
