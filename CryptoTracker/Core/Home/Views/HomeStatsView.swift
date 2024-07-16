bc14215093d0b3002d5d26dc72671eb108897662
krowqa2k <105343696+krowqa2k@users.noreply.github.com>
2024-07-16 14:11:50 +0200

    feat
    added statistic viewmodel

---
+++ CryptoTracker/Core/Home/Views/HomeStatsView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeStatsView.swift
+//  CryptoTracker
+//
+//  Created by admin on 16/07/2024.
+//
+
+import SwiftUI
+
+struct HomeStatsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeStatsView()
+}
---
+++ CryptoTracker/Core/StatisticView.swift
@@ -0,0 +1,18 @@
+//
+//  StatisticView.swift
+//  CryptoTracker
+//
+//  Created by admin on 16/07/2024.
+//
+
+import SwiftUI
+
+struct StatisticView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StatisticView()
+}
