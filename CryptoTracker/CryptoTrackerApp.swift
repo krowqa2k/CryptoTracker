17a7e5403a9d614a0821b3b62349457d801911ca
krowqa2k <105343696+krowqa2k@users.noreply.github.com>
2024-07-20 18:23:41 +0200

    feat
    added loading view

--- CryptoTracker/CryptoTrackerApp.swift
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -11,6 +11,7 @@ import SwiftUI
 struct CryptoTrackerApp: App {
     
     @StateObject private var vm = HomeViewModel()
+    @State private var showLaunchView: Bool = true
     
     init() {
         UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor : UIColor(Color.theme.accent)]
@@ -19,10 +20,23 @@ struct CryptoTrackerApp: App {
     
     var body: some Scene {
         WindowGroup {
-            NavigationStack {
-                HomeView()
+            ZStack {
+                ZStack {
+                    if showLaunchView {
+                        LaunchView(showLaunchView: $showLaunchView)
+                            .transition(.move(edge: .leading))
+                    }
+                }
+                .zIndex(2.0)
+                
+                ZStack {
+                    NavigationStack {
+                        HomeView()
+                            .navigationBarHidden(true)
+                    }
+                }
+                .environmentObject(vm)
             }
-            .environmentObject(vm)
         }
     }
 }
